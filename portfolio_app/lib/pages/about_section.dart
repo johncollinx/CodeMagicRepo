dd502c03886b70a9ea030ce5864475088841bdde
Climel <55754935+johncollinx@users.noreply.github.com>
2025-10-17 18:33:04 +0100

    Update about_section.dart

--- portfolio_app/lib/pages/about_section.dart
+++ portfolio_app/lib/pages/about_section.dart
@@ -19,8 +19,10 @@ class AboutSection extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              "Hi, I'm John Doe — a Flutter developer with a passion for building beautiful and functional mobile apps. I specialize in cross-platform development and enjoy UI/UX design.",
-              style: TextStyle(fontSize: 16),
+             Hi, I’m John Climel — a passionate web developer with entrepreneurial spirit and a constant drive to learn and grow. I’ve recently expanded into Flutter development, building beautiful, functional cross-platform mobile apps. I specialize in UI/UX design and love creating digital experiences that are both intuitive and impactful.
+I hold a B.Sc. in Computer Science from the University of University. I'm also a strong advocate for equality and proudly align with feminist values. Beyond the code, I’m a free-spirited, fun-loving, and adventurous individual with a wide range of interests — from music, sports, and movies, to literature, science & tech, and the arts.
+Above all, I’m a people person, always looking for the good in others and eager to connect, collaborate, and create.
+ style: TextStyle(fontSize: 16),
             ),
           ],
         ),
